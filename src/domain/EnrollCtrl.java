3a6fa5683203076a95c692378b51746ea420bd9c
omigo2000 <omid.bodaghi79@gmail.com>
2021-06-25 18:08:19 +0430

    Rename some arguments

--- src/domain/EnrollCtrl.java
+++ src/domain/EnrollCtrl.java
@@ -9,21 +9,20 @@ public class EnrollCtrl {
 
     List<String> errors = new ArrayList<>();
 
-	public void enroll(Student s, List<EnrollOffering> enrollOfferings) {
-        checkAlreadyPassedCourse(enrollOfferings, s);
-        checkPassedPrerequisites(enrollOfferings, s);
+	public void enroll(Student student, List<EnrollOffering> enrollOfferings) {
+        checkAlreadyPassedCourse(enrollOfferings, student);
+        checkPassedPrerequisites(enrollOfferings, student);
         checkConflictingExamTimes(enrollOfferings);
         checkTakenTwiceCourse(enrollOfferings);
-        checkGPALimit(s, enrollOfferings);
-        if(errors.size() == 0) enrollOfferings.forEach(o -> s.takeCourse(o.getCourse(), o.getSection()));
+        checkGPALimit(student, enrollOfferings);
+        if(errors.size() == 0) enrollOfferings.forEach(o -> student.takeCourse(o.getCourse(), o.getSection()));
         else showErrors();
 	}
 
-    private void checkGPALimit(Student s, List<EnrollOffering> enrollOfferings) {
+    private void checkGPALimit(Student student, List<EnrollOffering> enrollOfferings) {
         int unitsRequested = enrollOfferings.stream().mapToInt(o -> o.getCourse().getUnits()).sum();
-        if (!s.checkGPALimit(unitsRequested))
-            errors.add(String.format("%s: Number of units (%d) requested does not match GPA of %f", ErrorsTag.GPA_LIMIT_ERROR, unitsRequested, s.getGpa()));
-
+        if (!student.checkGPALimit(unitsRequested))
+            errors.add(String.format("%s: Number of units (%d) requested does not match GPA of %f", ErrorsTag.GPA_LIMIT_ERROR, unitsRequested, student.getGpa()));
     }
 
     private void checkTakenTwiceCourse(List<EnrollOffering> enrollOfferings) {
@@ -44,29 +43,27 @@ public class EnrollCtrl {
                     continue;
                 if (offering.getExamTime().equals(offering2.getExamTime()))
                     errors.add(String.format("%s: Two offerings %s and %s have the same exam time", ErrorsTag.EXAM_TIMES_ERROR, offering, offering2));
-
-
             }
         }
     }
 
-    private void checkPassedPrerequisites(List<EnrollOffering> enrollOfferings, Student s) {
+    private void checkPassedPrerequisites(List<EnrollOffering> enrollOfferings, Student student) {
 	    for (EnrollOffering offering : enrollOfferings) {
-            String violatingPre = offering.getCourse().getViolatingPrerequisite(s);
+            String violatingPre = offering.getCourse().getViolatingPrerequisite(student);
             if (!violatingPre.equals(""))
                 errors.add(String.format("%s: The student has not passed %s as a prerequisite of %s", ErrorsTag.PASSED_PREREQUISITES_ERROR, violatingPre, offering.getCourse().getName()));
         }
     }
 
-    private void checkAlreadyPassedCourse(List<EnrollOffering> enrollOfferings, Student s){
+    private void checkAlreadyPassedCourse(List<EnrollOffering> enrollOfferings, Student student){
         for (EnrollOffering offering : enrollOfferings) {
-            if (s.hasPassed(offering.getCourse()))
+            if (student.hasPassed(offering.getCourse()))
                 errors.add(String.format("%s: The student has already passed %s", ErrorsTag.ALREADY_PASSED_ERROR, offering.getCourse().getName()));
         }
     }
 
     private void showErrors(){
-        errors.forEach(e -> System.out.println(e));
+        errors.forEach(err -> System.out.println(err));
     }
 
     public List<String> getErrors() {
